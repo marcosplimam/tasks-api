cfd54e93b9d798f8bd64f2a2b8c6d24436d13757
Marcos Paulo Lima <marcospaulolimamorais@gmail.com>
2024-01-05 11:45:54 -0300

    Repositories and Services

---
+++ src/main/java/com/kim/tasksapi/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kim.tasksapi.repositories;
+
+import com.kim.tasksapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
---
+++ src/main/java/com/kim/tasksapi/services/UserService.java
@@ -0,0 +1,51 @@
+package com.kim.tasksapi.services;
+
+
+import com.kim.tasksapi.models.User;
+import com.kim.tasksapi.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "User not found! Id: " + id + ", Type: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't delete since there's related entities!");
+        }
+    }
+
+
+}
